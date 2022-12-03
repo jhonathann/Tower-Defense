4a2a614c9728b8e82f2b27f79237a929f4c22254
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2022-12-02 23:42:45 -0500

    feat(Added the tower manager scene): Added the tower manager scene to the proyect and the correspondig button to access it from the menu.
    -Tower Manager Scene added.
    -New Button on the startPage.
    -Some code comments refactor using xml documentaction
    
    V(0.0.31)

--- Assets/Scripts/StartPageController.cs
+++ Assets/Scripts/StartPageController.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
-
-//This Script controlls the behaviour of the start page in the starter page
+/// <summary>
+/// This Script controlls the behaviour of the start page in the starter page
+/// </summary>
 public class StartPageController : MonoBehaviour
 {
      private UIDocument startPage;
      private Button startButton;
+     private Button selectTowersButton;
      private Button exitButton;
-
      private void OnEnable()
      {
           getElements();
           registerEvents();
      }
-
      private void OnDisable()
      {
           unregisterEvents();
      }
-
-     //Gets the UI elements from the panel 
+     /// <summary>
+     /// Gets the UI elements from the panel 
+     /// </summary>
      private void getElements()
      {
           //Get the UI component (the element that has the script attached)
@@ -31,29 +32,47 @@ public class StartPageController : MonoBehaviour
           //Search for the buttons inside the menu
           //.Query search for especific elements in the visual tree (short .Q) in this case by the name of the element
           startButton = startPage.rootVisualElement.Query<Button>("StartButton");
+          selectTowersButton = startPage.rootVisualElement.Query<Button>("SelectTowersButton");
           exitButton = startPage.rootVisualElement.Query<Button>("ExitButton");
      }
-
-     //Sets the buttons to trigger certain functions as a response to an especific event
+     /// <summary>
+     /// Sets the buttons to trigger certain functions as a response to an especific event
+     /// </summary>
      private void registerEvents()
      {
           startButton.RegisterCallback<ClickEvent>(startButtonOnClick);
+          selectTowersButton.RegisterCallback<ClickEvent>(selectTowersButtonOnClick);
           exitButton.RegisterCallback<ClickEvent>(exitButtonOnClick);
      }
-     //Removes the callbacks from the buttons (when the UI document gets disabled)
+     /// <summary>
+     /// Removes the callbacks from the buttons (when the UI document gets disabled)
+     /// </summary>
      private void unregisterEvents()
      {
           startButton.UnregisterCallback<ClickEvent>(startButtonOnClick);
+          selectTowersButton.UnregisterCallback<ClickEvent>(selectTowersButtonOnClick);
           exitButton.UnregisterCallback<ClickEvent>(exitButtonOnClick);
      }
-
-     //Function that triggers when the startButton gets clicked
+     /// <summary>
+     /// Function that triggers when the startButton gets clicked
+     /// </summary>
+     /// <param name="evt"> Click event that triggered the callback</param>
      private void startButtonOnClick(ClickEvent evt)
      {
           SceneManager.LoadSceneAsync("Game"); //Make sure the scene is already in the build settings
      }
-
-     //Function that triggers when the exitButton gets clicked
+     /// <summary>
+     /// Function that triggers when the selectTowersButton gets clicked
+     /// </summary>
+     /// <param name="evt"> Click event that triggered the callback</param>
+     private void selectTowersButtonOnClick(ClickEvent evt)
+     {
+          SceneManager.LoadSceneAsync("TowerManager"); //Make sure the scene is already in the build settings
+     }
+     /// <summary>
+     /// //Function that triggers when the exitButton gets clicked
+     /// </summary>
+     /// <param name="evt">Click event that triggered the callback</param>
      private void exitButtonOnClick(ClickEvent evt)
      {
           //Closes the game

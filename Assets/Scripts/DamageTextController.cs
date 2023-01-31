2227487d070b12a35f21178b1b40dae6db416b83
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2023-01-30 20:10:39 -0500

    feat(healthBar and damage text Rotation): Modified the rotations of the healthbar and the damagetext so they arew always facing the camera in the y axis V(0.0.82)

--- Assets/Scripts/DamageTextController.cs
+++ Assets/Scripts/DamageTextController.cs
@@ -14,9 +14,25 @@ public class DamageTextController : MonoBehaviour
           textMesh = this.GetComponent<TextMeshPro>();
           Destroy(this.gameObject, 0.5f);
      }
-
+     private void Update()
+     {
+          RotateTowardsCameraInYAxis();
+     }
      public void SetUp(float damageAmount)
      {
           textMesh.SetText(damageAmount.ToString());
+     }
+     /// <summary>
+     /// Rotates the text in the Y axis to always look at the camera
+     /// </summary>
+     private void RotateTowardsCameraInYAxis()
+     {
+          transform.LookAt(Camera.main.transform);
+          Vector3 rotationJustInY = transform.rotation.eulerAngles;
+          rotationJustInY.x = 0;
+          rotationJustInY.z = 0;
+          ///Adds 180 degress to the rotation because of the layout of textMeshPro
+          rotationJustInY.y = transform.rotation.eulerAngles.y + 180;
+          transform.rotation = Quaternion.Euler(rotationJustInY);
      }
 }
--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -25,5 +25,20 @@ public class HealthBarController : MonoBehaviour
      {
           float health = enemy.health;
           healthBar.fillAmount = health / 100;//100 is the max current enemy health
+          RotateTowardsCameraInYAxis();
+     }
+
+     /// <summary>
+     /// Rotates the gameBar in the Y axis to always look at the camera
+     /// </summary>
+     private void RotateTowardsCameraInYAxis()
+     {
+          transform.LookAt(Camera.main.transform);
+          Vector3 rotationJustInY = transform.rotation.eulerAngles;
+          rotationJustInY.x = 0;
+          rotationJustInY.z = 0;
+          ///Adds 180 degress to the rotation because of the layout of textMeshPro
+          rotationJustInY.y = transform.rotation.eulerAngles.y + 180;
+          transform.rotation = Quaternion.Euler(rotationJustInY);
      }
 }

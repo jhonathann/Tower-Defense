67383d050f1eb6dcbe8768df526b58be6938559d
Jhonathann A. Gómez V. <jhagomezve@unal.edu.co>
2023-09-19 17:37:09 -0500

    (Camera Controller): Modified the camera controller to work with the ortographic camera
    -Minor structure models change.
    -HUD tutorial update the reflect the controller changes.
    -Change to the blinking shader so it doesnt cast shadows anymore.
    -Change the layer of UIWorld Elements so they are render in the correct way
    
    V(0.0.99)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,52 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 /// <summary>
-/// Controls the behaviour of the camera
+/// Controls the behavior of the camera.
 /// </summary>
 public class CameraController : MonoBehaviour
 {
-     private const float MOVEMENT_SPEED = 50.0f;
+     private const float MOVEMENT_SPEED = 100.0f;
      private const float ROTATION_SPEED = 50.0f;
-     private readonly Vector2 X_BOUNDARIES = new Vector2(50, 150);
-     private readonly Vector2 Y_BOUNDARIES = new Vector2(50, 150);
-     private readonly Vector2 Z_BOUNDARIES = new Vector2(-150, 50);
-     private readonly Vector2 ROTATION_BOUNDARIES = new Vector2(-45, 45);
+     private const float ZOOM_SENSITIVITY = 10.0f;
+     private readonly Vector2 X_BOUNDARIES = new(-300, 300);
+     private readonly Vector2 Z_BOUNDARIES = new(-300, 300);
+     private readonly Vector2 ZOOM_BOUNDARIES = new(10, 130);
+     private Camera ortografic_Camera;
+
+     private void Start()
+     {
+          ortografic_Camera = this.gameObject.GetComponent<Camera>();
+     }
+
      private void Update()
      {
           Move();
+          Zoom();
           Rotate();
      }
+
+     /// <summary>
+     /// Handles camera movement.
+     /// </summary>
      private void Move()
      {
-
-          GetInputs(out float movementInX, out float movementInY, out float movementInZ);
-          MakeMovement(movementInX, movementInY, movementInZ);
+          GetInputs(out float movementInX, out float movementInZ);
+          MakeMovement(movementInX, movementInZ);
           CorrectMovementWithBoundaries();
 
-          static void GetInputs(out float movementInX, out float movementInY, out float movementInZ)
+          static void GetInputs(out float movementInX, out float movementInZ)
           {
                movementInX = Input.GetAxis("Horizontal") * Time.deltaTime * MOVEMENT_SPEED;
-               movementInY = Input.GetAxis("Jump") * Time.deltaTime * MOVEMENT_SPEED;
                movementInZ = Input.GetAxis("Vertical") * Time.deltaTime * MOVEMENT_SPEED;
           }
 
-          void MakeMovement(float movementInX, float movementInY, float movementInZ)
+          void MakeMovement(float movementInX, float movementInZ)
           {
-               this.gameObject.transform.Translate(new Vector3(movementInX, movementInY, movementInZ), Space.World);
+               // Get the camera angle and calculate isometric movement vectors.
+               float angle = -1 * this.transform.eulerAngles.y * Mathf.Deg2Rad;
+               Vector3 isometricXDirection = new(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+               Vector3 isometricZDirection = new(-1 * Mathf.Sin(angle), 0, Mathf.Cos(angle));
+
+               // Translate the camera based on movement inputs.
+               this.gameObject.transform.Translate(movementInX * isometricXDirection, Space.World);
+               this.gameObject.transform.Translate(movementInZ * isometricZDirection, Space.World);
           }
 
           void CorrectMovementWithBoundaries()
           {
                this.transform.position = CheckForPositionBoundaries(this.transform.position);
+
+               Vector3 CheckForPositionBoundaries(Vector3 position)
+               {
+                    if (position.x < X_BOUNDARIES.x) position.x = X_BOUNDARIES.x;
+                    if (position.x > X_BOUNDARIES.y) position.x = X_BOUNDARIES.y;
+                    if (position.z < Z_BOUNDARIES.x) position.z = Z_BOUNDARIES.x;
+                    if (position.z > Z_BOUNDARIES.y) position.z = Z_BOUNDARIES.y;
+                    return position;
+               }
           }
      }
+
+     /// <summary>
+     /// Handles camera zooming.
+     /// </summary>
+     private void Zoom()
+     {
+          float zoom = GetZoomAmount();
+          ApplyZoom(zoom);
+          CorrectForBoundaries();
+
+          void ApplyZoom(float zoom)
+          {
+               ortografic_Camera.orthographicSize += zoom;
+          }
+
+          static float GetZoomAmount()
+          {
+               return Input.GetAxis("Mouse ScrollWheel") * ZOOM_SENSITIVITY * -1; // The *-1 is because the zoom in is a reduction in orthographic size.
+          }
+
+          void CorrectForBoundaries()
+          {
+               if (ortografic_Camera.orthographicSize < ZOOM_BOUNDARIES.x)
+                    ortografic_Camera.orthographicSize = ZOOM_BOUNDARIES.x;
+               if (ortografic_Camera.orthographicSize > ZOOM_BOUNDARIES.y)
+                    ortografic_Camera.orthographicSize = ZOOM_BOUNDARIES.y;
+          }
+     }
+
+     /// <summary>
+     /// Handles camera rotation.
+     /// </summary>
      private void Rotate()
      {
           float rotation = GetInput();
           MakeRotation(rotation);
-          CorrectRotationWithBoundaries();
 
           static float GetInput()
           {
@@ -55,47 +113,7 @@ public class CameraController : MonoBehaviour
 
           void MakeRotation(float rotation)
           {
-               this.gameObject.transform.Rotate(new Vector3(0, rotation, 0), Space.World);
-          }
-
-          void CorrectRotationWithBoundaries()
-          {
-               this.transform.rotation = CheckForRotationBoundaries(this.transform.rotation);
-          }
-     }
-
-     private Vector3 CheckForPositionBoundaries(Vector3 position)
-     {
-          if (position.x < X_BOUNDARIES.x) position.x = X_BOUNDARIES.x;
-          if (position.x > X_BOUNDARIES.y) position.x = X_BOUNDARIES.y;
-          if (position.y < Y_BOUNDARIES.x) position.y = Y_BOUNDARIES.x;
-          if (position.y > Y_BOUNDARIES.y) position.y = Y_BOUNDARIES.y;
-          if (position.z < Z_BOUNDARIES.x) position.z = Z_BOUNDARIES.x;
-          if (position.z > Z_BOUNDARIES.y) position.z = Z_BOUNDARIES.y;
-          return position;
-     }
-
-     private Quaternion CheckForRotationBoundaries(Quaternion rotation)
-     {
-          Vector3 eulerRotation = rotation.eulerAngles;
-          eulerRotation = ChangeAnglesRange(eulerRotation);
-          if (eulerRotation.y < ROTATION_BOUNDARIES.x) eulerRotation.y = ROTATION_BOUNDARIES.x;
-          if (eulerRotation.y > ROTATION_BOUNDARIES.y) eulerRotation.y = ROTATION_BOUNDARIES.y;
-          return Quaternion.Euler(eulerRotation);
-
-
-          /// <summary>
-          /// Changes the range of the eulerAngles from [0,360] to [-180,180]
-          /// </summary>
-          /// <param name="eulerRotation">eulerRotation to be changed</param>
-          /// <returns>The eulerAngles within the new range</returns>
-          static Vector3 ChangeAnglesRange(Vector3 eulerRotation)
-          {
-               for (int i = 0; i < 3; i++)
-               {
-                    eulerRotation[i] = Mathf.Repeat(eulerRotation[i] + 180.0f, 360.0f) - 180.0f;
-               }
-               return eulerRotation;
+               this.gameObject.transform.Rotate(new Vector3(0, rotation, 0), Space.World);//Only rotates in y axis for correct isometric control
           }
      }
 }
